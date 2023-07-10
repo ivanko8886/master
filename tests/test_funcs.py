3be10f19e8bf1604c427cdc5d2b6489afdfa8195
ivanko8886 <137064779+ivanko8886@users.noreply.github.com>
2023-07-11 01:19:43 +0300

    Update test_funcs.py

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -1,57 +1,86 @@
-import pytest
+from datetime import datetime
+import json
 
 from utils import funcs
-import requests
+def test_get_data():
+    data = funcs.get_data()
+    assert isinstance(data, list)
 
+def test_executed_operations():
+    result = funcs.executed_operations()
+    assert isinstance(result, list)
+    assert all(operation.get('state', '').lower() == 'executed' for operation in result)
 
-@pytest.fixture
-def words():
-    response = requests.get("https://api.npoint.io/094d3a7a0d9569218a14")
-    words_1 = response.json()
-    return words_1
-
-
-def test_get_data(words):
-    """Test function get_data"""
-    assert type(funcs.get_data()) == type(words)
-    assert funcs.get_data() is not None
-
-
-def test_executed_operations(words):
-    """Test function executed_operations"""
-    assert isinstance(funcs.executed_operations(), list)
-    assert funcs.executed_operations() is not None
-
-
-def test_sort_data(words):
-    """Test function sort_data"""
-    executed_operations = funcs.executed_operations()
-    assert isinstance(funcs.sort_data(), list)
-    assert funcs.sort_data() != executed_operations
-
+def test_sort_data():
+    result = funcs.sort_data()
+    assert isinstance(result, list)
+    assert all(isinstance(operation['date'], str) for operation in result)
+    sorted_result = sorted(result, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%S.%f'), reverse=True)
+    assert result == sorted_result
 
 def test_last_operations():
-    """Test function five last operations"""
-    sort_ed = funcs.last_operations()
-    assert len(sort_ed[:5]) == 5
-    assert isinstance(sort_ed[:5], list)
-
+    result = funcs.last_operations()
+    assert isinstance(result, list)
+    assert len(result) <= 5
+    assert all(operation in funcs.sort_data()[:5] for operation in result)
 
 def test_hiding_card():
-    """Test hiding_card"""
-    last_five_operations = funcs.last_operations()[:5]
-    assert isinstance(funcs.hiding_card(last_five_operations), list)
-    for k in last_five_operations:
-        assert isinstance(k, dict)
-
+    data = [
+        {'description': 'Test 1', 'from': '1234567890123456', 'to': '1234567890123456'},
+        {'description': 'Test 2', 'from': '12345678901234567890', 'to': '12345678901234567890'},
+    ]
+    result = funcs.hiding_card(data)
+    assert isinstance(result, list)
+    assert all('123456**********' in operation['from'] for operation in result)
+    assert all('123456**********' in operation['to'] for operation in result)
 
 def test_date_new():
-    """Test date_new"""
-    last_five_operations = funcs.last_operations()[:5]
-    assert isinstance(funcs.date_new(last_five_operations), list)
+    data = [{'date': '2022-01-01T00:00:00.000000', 'description': 'Test'}]
+    result = funcs.date_new(data)
+    assert isinstance(result, list)
+    assert all(operation['date'] == '01.01.2022' for operation in result)
 
-
-def test_result_output():
-    """Test result_output"""
-    last_five_operations = funcs.last_operations()
-    assert funcs.result_output(last_five_operations) is True
+def test_result_output(capsys):
+    data = [
+        {'date': '2022-01-01T00:00:00.000000', 'description': 'Test', 'from': '1234567890123456', 'to': '1234567890123456'},
+        {'date': '2022-01-02T00:00:00.000000', 'description': 'Test', 'from': '1234567890123456', 'to': '1234567890123456'},
+    ]
+    funcs.result_output(data)
+    captured = capsys.readouterr()
+    expected_output = "01.01.2022\n02.01.2022\n"
+    assert captured.out == expected_output
+'''''схожий пример
+    import pytest
+    import json
+    import datetime
+    from utils import funcs
+    @pytest.fixture
+    def operations_list():
+        """Provide some operations from operations.json for each test."""
+        return [{'state': 'executed', 'date': '2022-07-12T14:30:10.123', 'description': 'перевод', 'from': 'test_from', 'to': 'test_to', 'operationAmount': {'amount': 10, 'currency': {'name': 'USD'}}}]
+    
+    @pytest.fixture
+    def operations_dict():
+        """Provides single operations dictionary"""
+        return {'state': 'executed', 'date': '2022-07-12T14:30:10.123', 'description': 'перевод', 'from': 'test_from', 'to': 'test_to', 'operationAmount': {'amount': 10, 'currency': {'name': 'USD'}}}
+    
+    
+    def test_executed_operations(operations_list):
+        with open('operations.json', 'w', encoding='utf-8') as file:
+            json.dump(operations_list, file)
+        assert funcs.executed_operations() == operations_list
+    
+    def test_sort_data(operations_list):
+        with open('operations.json', 'w', encoding='utf-8') as file:
+            json.dump(operations_list, file)
+        assert funcs.sort_data() == operations_list
+    
+    def test_hiding_card(operations_dict):
+        operations_list = [operations_dict]
+        assert funcs.hiding_card(operations_list) == [{'state': 'executed', 'date': '2022-07-12T14:30:10.123', 'description': 'перевод', 'from': '****om', 'to': 'te**o', 'operationAmount': {'amount': 10, 'currency': {'name': 'USD'}}}]
+    
+    def test_date_new(operations_dict):
+        operations_list = [operations_dict]
+        funcs.hiding_card(operations_list)
+        assert funcs.date_new(operations_list) == [{'state': 'executed', 'date': '12.07.2022', 'description': 'перевод', 'from': '****om', 'to': 'te**o', 'operationAmount': {'amount': 10, 'currency': {'name': 'USD'}}}]
+'''''
